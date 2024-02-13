df95306ae5d1851454d313cc3580b0d4b062af8d
superfish058 <1079685992@qq.com>
2024-02-13 17:54:59 +0800

    mobileMode-add

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="baseBox">
     <!-- 顶部导航 -->
-    <div class="topArea">
+    <div class="topArea" v-if="mode == 'PC'">
       <div class="logo" @click="turnHome">DARKLOADER</div>
     </div>
-    <div class="main">
+    <div class="main" v-if="mode == 'PC'">
       <div class="sideMenu">
         <sideMenu @nodeClick="nodeClick" :menus="menus"></sideMenu>
       </div>
@@ -12,79 +12,201 @@
         <router-view></router-view>
       </div>
     </div>
+    <div class="sub" v-else>
+      <div class="loginPage" v-if="!isFish">
+        <div class="title">
+          DARKLOADER
+        </div>
+        <div class="account">
+          <div class="name">账号：</div>
+          <div class="value">
+            <el-input v-model="account"></el-input>
+          </div>
+        </div>
+        <div class="password">
+          <div class="name">密码</div>
+          <div class="value">
+            <el-input v-model="password"></el-input>
+          </div>
+        </div>
+        <div class="btns">
+          <div class="login" @click="handleLogin">登录</div>
+        </div>
+      </div>
+
+    </div>
   </div>
 </template>
 
 <script>
-import sideMenu from "@/components/sideMenu.vue";
-export default {
-  data() {
-    return {
-      count: 0,
-      set: 1,
-      menus: [
-        {
-          name: "网站资源",
-          url: "/collection",
-          icon: "Collection",
-        },
-        {
-          name: "哲理感悟",
-          url: "/article",
-          icon: "Tickets",
-        },
-        {
-          name: "模块测试",
-          url: "/games",
-          icon: "SwitchFilled",
-        },
-      ],
-    };
-  },
-  components: {
-    sideMenu,
-  },
-  mounted() {},
-  methods: {
-    turnHome() {
-      this.$router.push("/");
+  import { ElMessage, ElMessageBox } from 'element-plus'
+  import sideMenu from "@/components/sideMenu.vue";
+  export default {
+    data() {
+      return {
+        isFish: false,
+        account: 'darkLoader',
+        password: '',
+        mode: 'PC',
+        count: 0,
+        set: 1,
+        menus: [
+          {
+            name: "网站资源",
+            url: "/collection",
+            icon: "Collection",
+          },
+          {
+            name: "哲理感悟",
+            url: "/article",
+            icon: "Tickets",
+          },
+          {
+            name: "模块测试",
+            url: "/games",
+            icon: "SwitchFilled",
+          },
+        ],
+      };
     },
-    nodeClick(val) {
-      this.$router.push(val.url);
+    components: {
+      sideMenu,
     },
-  },
-};
+    mounted() {
+      this.judgeMobile()
+      window.onresize = () => {
+        this.judgeMobile()
+      }
+    },
+    methods: {
+      handleLogin(type = 0) {
+        if (this.password == 'superfish058') {
+          if (type == 1) {
+            return this.isFish = true
+          }
+          localStorage.setItem('password', this.password)
+          this.isFish = true
+          ElMessage({
+            message: '登录成功',
+            type: 'success',
+          })
+        } else {
+          ElMessage({
+            message: '登录失败',
+            type: 'info',
+          })
+        }
+      },
+      readPassword() {
+        let password = localStorage.getItem('password')
+        if (password) {
+          this.password = password
+          this.handleLogin(1)
+        }
+        console.log(password);
+      },
+      judgeMobile() {
+        let width = document.body.clientWidth
+        if (width <= 800) {
+          this.mode = 'mobile'
+          this.readPassword()
+        } else {
+          this.mode = 'PC'
+        }
+      },
+      turnHome() {
+        this.$router.push("/");
+      },
+      nodeClick(val) {
+        this.$router.push(val.url);
+      },
+    },
+  };
 </script>
 
 <style scss scoped>
-.baseBox {
-  color: #333;
-  height: 100%;
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  .topArea {
-    height: 50px;
+  .baseBox {
+    color: #333;
+    height: 100%;
+    width: 100%;
     display: flex;
-    align-items: center;
-    justify-content: space-between;
-    box-shadow: 0px 2px 5px 0px rgba(188, 189, 192, 0.2);
-    padding: 0 10px;
-    .logo {
-      font-size: 22px;
-      font-weight: 700;
-      cursor: pointer;
-      user-select: none;
+    flex-direction: column;
+
+    .topArea {
+      height: 50px;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      box-shadow: 0px 2px 5px 0px rgba(188, 189, 192, 0.2);
+      padding: 0 10px;
+
+      .logo {
+        font-size: 22px;
+        font-weight: 700;
+        cursor: pointer;
+        user-select: none;
+      }
     }
-  }
-  .main {
-    flex: 1;
-    display: flex;
-    .infoArea {
+
+    .main {
       flex: 1;
+      display: flex;
+
+      .infoArea {
+        flex: 1;
+        box-sizing: border-box;
+        padding: 20px;
+      }
+    }
+
+    .sub {
+      flex: 1;
+      display: flex;
       box-sizing: border-box;
       padding: 20px;
+      flex-direction: column;
+      padding-top: 40px;
+
+      .title {
+        font-size: 28px;
+        font-weight: 700;
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        margin-bottom: 40px;
+      }
+
+      .account,
+      .password {
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+        margin-bottom: 30px;
+
+      }
+
+      .btns {
+        display: flex;
+        width: 100%;
+        justify-content: center;
+
+
+        .login {
+          margin-top: 30px;
+          width: 90%;
+          height: 45px;
+          border-radius: 4px;
+          display: flex;
+          background-color: #42b883;
+          justify-content: center;
+          align-items: center;
+          font-size: 18px;
+          color: #fff;
+          box-shadow: 0px 1px 5px 0px rgba(188, 189, 192, 0.2);
+
+
+        }
+      }
     }
   }
-}
 </style>
